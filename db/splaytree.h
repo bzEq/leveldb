eed5e3b1ae96f16e932ee3a42ad2d2cdb89524bd
Kai Luo <gluokai@gmail.com>
2016-12-21 15:46:22 +0800

    minor fix

--- db/splaytree.h
+++ db/splaytree.h
@@ -131,7 +131,6 @@ inline void SplayTree<Key, Comparator>::Insert(const Key &key) {
       current = &((*current)->child[kLeft]);
     }
   }
-  ++size_;
   insert_guard.Unlock();
 
   port::RWLockWRGuard splay_guard(&rwlock_);
@@ -140,6 +139,7 @@ inline void SplayTree<Key, Comparator>::Insert(const Key &key) {
   // assert(rwlock_.NumOfWriters() == 1);
   Splay(insert);
   insert->inserted = true;
+  ++size_;
   // assert(root_ == insert);
   // assert(root_->parent == nullptr);
 }
